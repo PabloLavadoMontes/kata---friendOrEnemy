8464d64764e22a1f5a6bb4d6642dab8f004297db
PLM <pablolavado08@gmail.com>
2021-05-27 13:42:53 +0200

    KATAS DONE!

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export {friend, checksString}
+
+/**
+ * "Filters a list of strings and returns a list with only your friends"
+ * @param {string[]} friends 
+ * @returns {string[]} realFriends
+ */
+function friend (friends: string[]): string[] {
+    let realFriends: string[] = [];
+    friends.forEach(friend => {
+    if (checksString(friend) && realFriends.includes(friend) === false) {
+        realFriends.push(friend);
+    }
+    }) 
+    return realFriends;
+}
+
+/**
+ * Checks the value of a string
+ * @param {string} name 
+ * @returns {boolean} 
+ */
+function checksString (name: string): boolean {
+    const regex: RegExp = /^[a-zA-Z]+$/;
+    return name.length !== 4 || !regex.test(name) ? false : true;
+}
---
+++ src/testing/jest.test.ts
@@ -0,0 +1,48 @@
+/* Examples:
+friend(["Ryan", "Kieran", "Jason", "Yous"]) => ["Ryan", "Yous"];
+friend(["Ryan", "Kieran", "Mark"]) => ["Ryan", "Mark"];
+TODO: 
+-Make a program that filters a list of strings and returns a list with only your friends.
+-If a name has exactly 4 letters in it,it has to be a friend of yours
+-Otherwise you can be sure he's not...
+-Keep the original order of the names in the output. 
+-Control the repeating names
+-Control the numbers*/
+
+import {friend, checksString} from "../index"
+
+// Tests for friend()
+
+test("Filters a list of strings and returns a list with only your friends", () => {
+    expect(friend(["Ryan", "Kieran", "Jason", "Yous"])).toStrictEqual(["Ryan", "Yous"]);
+});
+test("Filters a list of strings and returns a list with only your friends", () => {
+    expect(friend(["PepitoBotella", "Juan", "John", "John", "Carlitos"])).toStrictEqual(["Juan", "John"]);
+});
+test("Filters a list of strings and returns a list with only your friends", () => {
+    expect(friend(["Ryan", "Kieran", "Mark"])).toStrictEqual(["Ryan", "Mark"]);
+});
+test("Filters a list of strings and returns a list with only your friends", () => {
+    expect(friend(["Ryan", "Kieran", "Fer", "Fernando", "Jason", "Yous"])).toStrictEqual(["Ryan", "Yous"]);
+});
+test("Filters a list of strings and returns a list with only your friends", () => {
+    expect(friend(["Ryan", "Kieran", "Jason", "Yous", "Joey"])).toStrictEqual(["Ryan", "Yous", "Joey"]);
+});
+
+
+// Tests for checksString()
+test("Checks the value of a string and returns false if contains a number, a empty string...", () => {
+    expect(checksString("1221")).toStrictEqual(false);
+});
+test("Checks the value of a string and returns false if contains a number, a empty string...", () => {
+    expect(checksString("")).toStrictEqual(false);
+});
+test("Checks the value of a string and returns false if contains a number, a empty string...", () => {
+    expect(checksString("p0p")).toStrictEqual(false);
+});
+test("Checks the value of a string and returns false if contains a number, a empty string...", () => {
+    expect(checksString("er3rer")).toStrictEqual(false);
+});
+test("Checks the value of a string and returns false if contains a number, a empty string...", () => {
+    expect(checksString("Ryan")).toStrictEqual(true);
+});
